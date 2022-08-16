a2520d5593632cf910e4d320aca04a62a60411c5
arthi2900 <aaarthi2927@gmail.com>
2022-08-16 21:29:57 +0530

    add

--- User.js
+++ User.js
@@ -4,10 +4,10 @@ import {client} from "./index.js";
 import { ObjectId } from "mongodb";
 import jwt  from "jsonwebtoken";
 import {auth} from "./auth.js";
-router.get("/home",async function(req,res){
+router.get("/home",auth,async function(req,res){
     const result =await client.db("Todo").collection("user")
     .findOne({});
-    res.send(result);
+    res.json(result);
 })
 router.get("/:id",async function(req,res){
     const {id}=req.params;
