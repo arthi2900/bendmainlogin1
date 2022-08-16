c54ce51b4317b72c38772539eca07ee6efed886a
arthi2900 <aaarthi2927@gmail.com>
2022-08-16 21:56:19 +0530

    add

--- User.js
+++ User.js
@@ -4,14 +4,19 @@ import {client} from "./index.js";
 import { ObjectId } from "mongodb";
 import jwt  from "jsonwebtoken";
 import {auth} from "./auth.js";
-
-
-router.get("/:id",auth,async function(req,res){
+router.get("/home",auth,async function(req,res){
+    const result =await client.db("Todo").collection("user")
+    .findOne({});
+    res.send(result);
+  })
+  
+router.get("/:id",async function(req,res){
     const {id}=req.params;
     const result =await client.db("Todo").collection("user")
     .findOne({_id:ObjectId(id)});
     res.send(result);
 })
+
 
 export const UserRouter=router;
 
