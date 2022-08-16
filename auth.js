db246dd6fae589956dd1e2c87fa919976d555b33
arthi2900 <aaarthi2927@gmail.com>
2022-08-16 22:38:19 +0530

    add

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ export const auth=(req,res,next)=>{
 try{
   const token=req.header("x-auth-token");
     console.log(token);
-    jwt.verify(token,process.env.SECRET_KEY);
+        jwt.verify(token,process.env.SECRET_KEY);
        next();
 }
 catch(err){
