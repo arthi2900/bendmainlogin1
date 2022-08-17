2b567ecd8ede494e47d2a7fd253cedb72d471806
arthi2900 <aaarthi2927@gmail.com>
2022-08-17 13:53:30 +0530

    add

--- AuthRouter.js
+++ AuthRouter.js
@@ -36,9 +36,9 @@ router.post("/login",async function(req,res){
     console.log("isPasswordMatch",isPasswordMatch);
 if(isPasswordMatch){
 const token=jwt.sign({id:userfromdb._id},process.env.SECRET_KEY);
-const user=username;
+const username=username;
 const id=userfromdb._id;
-res.send({message:"successful login",token:token,user:user,id:id});
+res.send({message:"successful login",token:token,username:username,id:id});
 }
 else{
 res.status(401).send({message:"Invalid credenitials"});
