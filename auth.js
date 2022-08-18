ff364fbec166b0222653b9710d86795ea5c7abc3
arthi2900 <aaarthi2927@gmail.com>
2022-08-18 17:11:13 +0530

    add

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 import jwt  from "jsonwebtoken";
 export const auth=(req,res,next)=>{ 
 try{
-  const token=req.header("x-auth-token");
+  const token=req.body||req.header("x-auth-token");
     console.log(token);
     const verify=jwt.verify(token,process.env.SECRET_KEY);
        if(verify){
