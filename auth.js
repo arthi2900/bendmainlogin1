85fcb1557519333b3ac649c88c1868ca94c20d02
arthi2900 <aaarthi2927@gmail.com>
2022-08-19 18:57:59 +0530

    add

--- auth.js
+++ auth.js
@@ -1,19 +1,12 @@
 import jwt  from "jsonwebtoken";
 export const auth=(req,res,next)=>{ 
-try{
-  const token=req.body||req.header("x-auth-token");
-    console.log(token);
-    const verify=jwt.verify(token,process.env.SECRET_KEY);
-       if(verify){
-        req.id=id;
-        console.log(req.id)
-        next();
-       }
-       else {
-        res.status(401).json({ message: "Unauthorized" });
-      }
-    
-}
+  try{
+    const token=req.header("x-auth-token");
+      console.log(token);
+      jwt.verify(token,process.env.SECRET_KEY);
+         next();
+  }
+
 catch(err){
 res.status(401).send({error:err.message});
 } 
